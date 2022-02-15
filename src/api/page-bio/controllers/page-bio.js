994d81b3d0c86c61aa445809d57a0455eecc2a09
Tancrède Simonin <tancrede.simonin.eco@gmail.com>
2022-02-15 19:09:36 +0100

    build(api): add blocks system, pageBio

---
+++ src/api/page-bio/controllers/page-bio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  page-bio controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::page-bio.page-bio');
---
+++ src/api/page-bio/routes/page-bio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-bio router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::page-bio.page-bio');
