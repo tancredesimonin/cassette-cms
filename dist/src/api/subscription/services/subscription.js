762988a2b23f2b8237ca49280c3beaa63cde6e78
Tancrède Simonin <tancrede.simonin.eco@gmail.com>
2022-06-01 12:07:58 +0200

    feat(api:subscription) add contact, subscription, list

---
+++ dist/src/api/subscription/services/subscription.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * subscription service.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::subscription.subscription');
---
+++ src/api/subscription-list/controllers/subscription-list.ts
@@ -0,0 +1,7 @@
+/**
+ *  subscription-list controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::subscription-list.subscription-list');
